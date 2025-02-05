0f35088218b98cfd26bc3da0df46c7ccae007744
amindev7 <amin.said.dev@gmail.com>
2025-02-05 21:20:29 +0100

    Test deepseek

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import OllamaClient from "./OllamaClient.js"
 const server = new HttpServer()
 const ollamaApi = new OllamaClient()
 
+// list local models
 server.get("/", async (req, res) => {
     try {
         const response = await ollamaApi.get("/api/tags")
@@ -11,6 +12,35 @@ server.get("/", async (req, res) => {
     } catch (err) {
         console.error(err)
         res.status(500).json({ error: "Failed to fetch models" })
+    }
+})
+
+const deepSeek = { model: "deepseek-r1:1.5b" }
+
+// pull modal
+server.get("/pull", async (req, res) => {
+    try {
+        const response = await ollamaApi.post("/api/pull", deepSeek)
+        res.json(response)
+    } catch (err) {
+        console.error(err)
+        res.status(500).json({ error: "Failed to fetch models" })
+    }
+})
+
+const prompt = {
+    model: "deepseek-r1:1.5b",
+    prompt: "Why is the sky blue?",
+    stream: false,
+}
+
+server.get("/generate", async (req, res) => {
+    try {
+        const response = await ollamaApi.post("/api/generate", prompt)
+        res.json(response)
+    } catch (err) {
+        console.error(err)
+        res.status(500).json({ error: "Failed to generate response" })
     }
 })
 
