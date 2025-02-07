ece6747ce5f5579a7de13675cfe03cbd1da67a1f
amindev7 <amin.said.dev@gmail.com>
2025-02-07 22:33:11 +0100

    Install packages

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    open: false,
-    port: 3000,
-  },
-});
+    plugins: [react(), tailwindcss()],
+    server: {
+        open: false,
+        port: 3000,
+    },
+})
