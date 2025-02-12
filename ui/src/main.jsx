41059dce54d86610f9193faa68d110635aa4b2d6
amindev7 <amin.said.dev@gmail.com>
2025-02-12 23:44:53 +0100

    Add defaultOptions

--- ui/src/main.jsx
+++ ui/src/main.jsx
@@ -6,7 +6,7 @@ import App from "./App.jsx"
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({ defaultOptions: { queries: { retry: 0 } } })
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
