d63a732920a4b3d0c9fe44097e7f9563de6f642f
amindev7 <amin.said.dev@gmail.com>
2025-02-16 00:54:59 +0100

    Rename to AuthController

---
+++ server/src/controllers/AuthController.js
@@ -0,0 +1,40 @@
+import HttpResponse from "../services/HttpResponse.js"
+import User from "../models/User.js"
+
+const AuthController = {
+    create: async (req, res) => {
+        const response = new HttpResponse(res)
+        const { email, password } = req.body
+
+        if (!email || !password) {
+            return response.badRequest({
+                error: "email, and password are required",
+            })
+        }
+
+        try {
+            const user = await User.create(email, password)
+            return response.created({ message: "User created", user })
+        } catch (error) {
+            return response.serverError({ error: error.message })
+        }
+    },
+
+    getByEmail: async (req, res) => {
+        const response = new HttpResponse(res)
+        const { email } = req.params
+
+        try {
+            const user = await User.getByEmail(email)
+            if (!user) {
+                return response.notFound({ error: "User not found" })
+            }
+
+            return response.ok(user)
+        } catch (error) {
+            return response.serverError({ error: error.message })
+        }
+    },
+}
+
+export default AuthController
