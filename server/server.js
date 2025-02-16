5250e32a249c3e376f14dfcebd23852c0fdba89d
amindev7 <amin.said.dev@gmail.com>
2025-02-16 19:27:11 +0100

    Use middleware before router

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
+import { rateLimiter, requestLogger } from "./src/core/middleware.js"
+
 import HttpServer from "./src/core/HttpServer.js"
 import auth from "./src/routes/auth.js"
 import initializeDB from "./src/db/index.js"
-import model from "./src/routes/model.js"
+import ollama from "./src/routes/ollama.js"
 
 const PORT = process.env.SERVER_PORT
 
@@ -10,8 +12,12 @@ const server = new HttpServer()
 async function startServer() {
     await initializeDB()
 
-    // Register routes
-    model(server)
+    // Middleware
+    server.useMiddleware(requestLogger)
+    server.useMiddleware(rateLimiter)
+
+    // Routes
+    ollama(server)
     auth(server)
 
     server.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`))
